8a4ae83ad7d3659b5f2c91b7430c1c0b517a0c5a
FehPrado <pradofelipe20032002@gmail.com>
2023-01-22 11:33:13 -0300

    database

--- database.php
+++ database.php
@@ -6,9 +6,9 @@ $PASS = '123456789';
 
 $conecta = mysqli_connect($HOST, $USER, $PASS, $DB);
 
-if(!$conecta){
-  echo "Error: Unble to connect to MySQL." . PHP_EOL;
-	echo "Debugging errno: " . mysqli_connect_errno(). PHP_EOL;
-	echo "Debugging error: " . mysqli_connect_error(). PHP_EOL;
-	exit();
-}
+// if(!$conecta){
+//   echo "Error: Unble to connect to MySQL." . PHP_EOL;
+// 	echo "Debugging errno: " . mysqli_connect_errno(). PHP_EOL;
+// 	echo "Debugging error: " . mysqli_connect_error(). PHP_EOL;
+// 	exit();
+// }

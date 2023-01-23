d1cdf94696a4d81889e7a40a566780e3f16e3869
pradofelipe <felipe.prado@compliasset.com>
2023-01-23 13:42:58 -0300

    new

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -9,7 +9,7 @@ include('../database.php')
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>fullturismo</title>
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
@@ -73,8 +73,12 @@ include('../database.php')
             <div class="card">
               <div class="card-body text-center">
                 <h5 class="card-title text-center">Usuários</h5>
-                <p class="card-text text-center">12</p>
-                <a href="#" class="btn btn-primary text-center">Ver Usuários</a>
+                <?php
+                  $queryuser = mysqli_query($conecta, "SELECT COUNT(id) AS TotalUser FROM users");
+                  $resultuser = mysqli_fetch_assoc($queryuser);
+                ?>
+                 <p class="card-text text-center"> <?php echo str_pad($resultuser['TotalUser'],2 , 0, STR_PAD_LEFT)?></p>
+                <a href="user/user.php" class="btn btn-primary text-center">Ver Usuários</a>
               </div>
             </div>
           </div>
